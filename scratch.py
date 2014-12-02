ed62065bc9e78c4f0934d3d1ef002b9b3cbc667b
jrbrodie77 <john.brodie@gmail.com>
2014-12-01 21:46:38 -0800

    Initial commit to github

---
+++ scratch.py
@@ -0,0 +1,21 @@
+# import csv
+#
+#
+# with open('vibedata.csv', 'rb') as f:
+#
+# file_contents = f.readlines()
+#
+# reader = csv.reader(file_contents, delimiter=',')
+# header=[]
+#
+# for row in range(8):
+# header.append(reader.next())
+#
+
+import sys
+
+sys.dont_write_bytecode = True
+
+from ni_vibration_lib import *
+
+ds = DataSet('vibedata.csv')
